dce65e03e3151f55282c688e8e8dd5e6256d61e8
Josue Pacheco <pca.josue@gmail.com>
2024-09-07 00:32:04 -0500

    challenge

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Post } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpStatus, Post, Query } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 const code: { name: string; code: string } = {
@@ -10,12 +10,23 @@ const code: { name: string; code: string } = {
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("status")
+
+  @Get("phase-change-diagram")
+  getPressure(@Query('pressure') pressure: string): any {
+    const pressureValue = parseFloat(pressure);
+    return {
+      "specific_volume_liquid": parseFloat((((pressureValue - 0.05)/4975)+0.0015).toFixed(4)),
+      "specific_volume_vapor": parseFloat(((10.00116 - pressureValue)/0.3317).toFixed(4))
+    }
+  }
+
+
+  @Get("/status")
   getStatus(): {damaged_system :string} {
     return {"damaged_system" : code.name}
   };
 
-  @Get("repair-bay")
+  @Get("/repair-bay")
   getPage():String{
     return `
       <!DOCTYPE html>
@@ -34,5 +45,7 @@ export class AppController {
   handleTeapot():void{
     throw new HttpException("I'm a teapot", HttpStatus.I_AM_A_TEAPOT);
   }
+
+  
 
 }
